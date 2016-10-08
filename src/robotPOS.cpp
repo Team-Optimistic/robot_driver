6e2a6a63993b76376bda46047e7b043869e14035
Octogonapus <firey45@gmail.com>
2016-10-08 13:14:11 -0400

    Added initial write method

--- src/robotPOS.cpp
+++ src/robotPOS.cpp
@@ -38,79 +38,95 @@
 
 float x = 0;
 
+namespace pos_driver
+{
+robotPOS::robotPOS(const std::string &port, uint32_t baud_rate, boost::asio::io_service &io)
+    : port_(port)
+    , baud_rate_(baud_rate)
+    , shutting_down_(false)
+    , serial_(io, port_)
+{
+    serial_.set_option(boost::asio::serial_port_base::baud_rate(baud_rate_));
+}
 
- namespace pos_driver {
- 	robotPOS::robotPOS(const std::string& port, uint32_t baud_rate, boost::asio::io_service& io): port_(port),
- 	baud_rate_(baud_rate), shutting_down_(false), serial_(io, port_) {
- 		serial_.set_option(boost::asio::serial_port_base::baud_rate(baud_rate_));
- 	}
+void robotPOS::poll(nav_msgs::Odometry *odom, sensor_msgs::Imu *imu)
+{
+    uint8_t start_count = 0;
+    bool got_pos = false;
+    boost::array<uint8_t, 13> raw_bytes;
+    boost::array<uint8_t, 2> out_data;
 
- 	void robotPOS::poll(nav_msgs::Odometry *odom, sensor_msgs::Imu *imu)
-  {
- 		uint8_t start_count = 0;
- 		bool got_pos = false;
- 		boost::array<uint8_t, 13> raw_bytes;
-
- 		int index;
- 		while (!shutting_down_ && !got_pos)
+    int index;
+    while(!shutting_down_ && !got_pos) 
     {
-	     // Wait until first data sync of frame: 0xFA, 0xA0
- 			boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[start_count],1));
- 			if(start_count == 0)
-      {
- 				if(raw_bytes[start_count] == 0xFA)
- 					start_count = 1;
-      }
- 			else if(start_count == 1)
-      {
-  			// Now that entire start sequence has been found, read in the rest of the message
- 				got_pos = true;
+        // Wait until first data sync of frame: 0xFA, 0xA0
+        boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[start_count], 1));
+        if(start_count == 0) 
+        {
+            if(raw_bytes[start_count] == 0xFA) start_count = 1;
+        }
+        else if(start_count == 1) 
+        {
+            // Now that entire start sequence has been found, read in the rest of the message
+            got_pos = true;
 
- 				boost::asio::read(serial_,boost::asio::buffer(&raw_bytes[1], 12));
- 				float theta =0; //read in
- 				float radians = theta * (M_PI/180);
+            // Read in rest of msg
+            boost::asio::read(serial_, boost::asio::buffer(&raw_bytes[1], 4));
 
- 				// for(int i = 0; i < 13; i++)
- 				// {
- 				// 	ROS_INFO("%X",raw_bytes[i]);
- 				// }
+            uint8_t msgCount = raw_bytes[0];
+            uint8_t intakePot = raw_bytes[1];
+            uint8_t leftQuad = raw_bytes[2];
+            uint8_t rightQuad = raw_bytes[3];
 
-        // Pose
-        odom->pose.pose.position.x = x;
-        odom->pose.pose.position.y = 0;
-        odom->pose.pose.position.z = 0;
+            float theta = 0; // read in
+            float radians = theta * (M_PI / 180);
 
-        odom->pose.pose.orientation.x = cos(radians/2);
-        odom->pose.pose.orientation.y = 0;
-        odom->pose.pose.orientation.z = 0;
-        odom->pose.pose.orientation.w = sin(radians/2);
+            // for(int i = 0; i < 13; i++)
+            // {
+            // 	ROS_INFO("%X",raw_bytes[i]);
+            // }
 
-        odom->pose.covariance = ODOM_POSE_COV_MAT;
+            // Pose
+            odom->pose.pose.position.x = x;
+            odom->pose.pose.position.y = 0;
+            odom->pose.pose.position.z = 0;
 
-        // Twist
-        odom->twist.twist.linear.x = 0.000125 * (ros::Time::now() - prevTime).toSec() / 1000;
-        odom->twist.twist.linear.y = 0;
-        odom->twist.twist.linear.z = 0;
+            odom->pose.pose.orientation.x = cos(radians / 2);
+            odom->pose.pose.orientation.y = 0;
+            odom->pose.pose.orientation.z = 0;
+            odom->pose.pose.orientation.w = sin(radians / 2);
 
-        odom->twist.twist.angular.x = 0;
-        odom->twist.twist.angular.y = 0;
-        odom->twist.twist.angular.z = 0;
+            odom->pose.covariance = ODOM_POSE_COV_MAT;
 
-        odom->twist.covariance = ODOM_TWIST_COV_MAT;
+            // Twist
+            odom->twist.twist.linear.x = 0.000125 * (ros::Time::now() - prevTime).toSec() / 1000;
+            odom->twist.twist.linear.y = 0;
+            odom->twist.twist.linear.z = 0;
 
-        //TODO: Fill imu message
+            odom->twist.twist.angular.x = 0;
+            odom->twist.twist.angular.y = 0;
+            odom->twist.twist.angular.z = 0;
 
-   			// 	pos->pose.position.x = x;
-   			// 	pos->pose.position.y = 0;
-   			// 	pos->pose.position.z = 0;
+            odom->twist.covariance = ODOM_TWIST_COV_MAT;
+            
+            out_data[0] = 42;
+            out_data[1] = 24;
+            
+            boost::asio::write(serial_,  boost::asio::buffer(&out_data[0], 2));
 
-   			// 	pos->pose.orientation.x =  cos(radians/2);
-   			// 	pos->pose.orientation.y =  0;
-   			// 	pos->pose.orientation.z =  0;
-   			// 	pos->pose.orientation.w =  sin(radians/2);
+            // TODO: Fill imu message
 
- 				x += 0.000125;
- 			}
- 		}
- 	}
- };
+            // 	pos->pose.position.x = x;
+            // 	pos->pose.position.y = 0;
+            // 	pos->pose.position.z = 0;
+
+            // 	pos->pose.orientation.x =  cos(radians/2);
+            // 	pos->pose.orientation.y =  0;
+            // 	pos->pose.orientation.z =  0;
+            // 	pos->pose.orientation.w =  sin(radians/2);
+
+            x += 0.000125;
+        }
+    }
+}
+};
