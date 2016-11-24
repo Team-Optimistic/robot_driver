b20802e3f01ddafee7ed72fa9cb99a54192617b3
Octogonapus <firey45@gmail.com>
2016-11-24 01:34:14 -0500

    Added async read

---
+++ src/blockingReader.c
@@ -0,0 +1,40 @@
+#include "robot_driver/blockingReader.h"
+
+void blockingReader::read_complete(const boost::system::error_code& error, size_t bytes_transferred)
+{
+  read_error = (error || bytes_transferred == 0);
+  timer.cancel();
+}
+
+void blockingReader::time_out(const boost::system::error_code& error)
+{
+  if (error)
+    return;
+
+  port.cancel();
+}
+
+const bool blockingReader::readNext(int8_t& val)
+{
+  val = 0;
+  c = 0;
+
+  port.get_io_service.reset();
+  boost::asio::async_read(port, boost::asio::buffer(&val, 1),
+    boost::bind(&blockingReader::read_complete,
+                this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+
+  timer.expires_from_now(boost::posix_time::milliseconds(timeout));
+  timer.async_wait(boost::bind(&blockingReader::time_out,
+                               this,
+                               boost::asio::placeholders::error));
+
+  port.get_io_service.run();
+
+  if (!read_error)
+    val = c;
+
+  return !read_error;
+}
